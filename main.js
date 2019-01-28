e2ad4a1abc5dff751de134d3354d3202e4e072f0
Tichomir Mitkov <MitkovTihomir@gmail.com>
2019-01-28 22:48:41 +0200

    ...

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
+    },
+    methods: {
+        contextMenu: function () {
+            console.log("contextMenu");
+        }
     }
 })
